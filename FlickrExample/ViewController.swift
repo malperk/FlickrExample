d99efd2ae293c9e600bad955debf39eb3b528020
Alper KARATAS <alper@alper.com>
2018-09-26 18:56:58 +0100

    Emty Project

---
+++ FlickrExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlickrExample
+//
+//  Created by Alper KARATAS on 9/26/18.
+//  Copyright © 2018 Alper KARATAS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
